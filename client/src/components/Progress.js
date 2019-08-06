2b2822c30b520ca17e5d8c97d21c1e27b87c82a9
w.benarto <w.benarto@gmail.com>
2019-08-05 20:24:48 -0700

    upload file successful

---
+++ client/src/components/Progress.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Progress = ({ percentage }) => {
+    return (
+        <div className="progress">
+            <div className="progress-bar bg-info" role="progressbar" style={{ width: `${percentage}%` }}>{percentage}%</div>
+            
+        </div>
+    )
+}
+
+export default Progress
