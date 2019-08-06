3e8d7227cbb9d5b1ae4f42200982ec7860d98b94
w.benarto <w.benarto@gmail.com>
2019-08-05 19:36:33 -0700

    frontend

---
+++ client/src/components/FileUpload.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+
+const FileUpload = () => {
+    return (
+        <Fragment>
+            <form>
+                <div className="custom-file">
+                    <input type="file" className="custom-file-input" id="customFile" />
+                    <label className="custom-file-label" for="customFile">Choose file</label>
+                </div>
+
+                <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4" />
+            </form>
+        </Fragment>
+    )
+}
+
+export default FileUpload
